45814fd872a8e11a84852d894eba7089f962fd35
cachoeira33 <74809664+cachoeira33@users.noreply.github.com>
2025-09-10 15:03:00 +0100

    Updated Header.tsx

--- web/src/components/layout/Header.tsx
+++ web/src/components/layout/Header.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-+import { Search, Globe } from 'lucide-react';
+import { Search, Globe } from 'lucide-react';
 import { Button } from '../ui/Button';
-+import { NotificationBell } from '../common/NotificationBell';
-+import { useNotifications } from '../../hooks/useNotifications';
+import { NotificationBell } from '../common/NotificationBell';
+import { useNotifications } from '../../hooks/useNotifications';
 
 export function Header() {
   const { i18n } = useTranslation();
